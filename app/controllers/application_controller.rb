7ec16e8ef86f7dec0c547539e7ce316186c69b0a
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-03-07 12:56:39 -0800

    Implemented skeleton for employee homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  def admin_types
+    ['AdminUser']
+  end
 
   private
 
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(root_path)
-  end
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(root_path)
+    end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.where(status: 'pending')
-    @recent_audit_items = AuditLog.last(9)
+    if admin_types.include?(current_user.type)
+      @pending_approvals = Post.pending
+      @recent_audit_items = AuditLog.last(9)
+    else
+
+    end
   end
 end
