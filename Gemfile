5016a8cd3a81b25353352b0aeca6a7a1d4df0fa2
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-03-07 08:47:17 -0800

    Implemented pagination for posts and audit logs

--- Gemfile
+++ Gemfile
@@ -45,5 +45,6 @@ gem 'administrate', '~> 0.9.0'
 gem 'bourbon', '~> 5.0'
 gem 'pundit', '~> 1.1'
 gem 'twilio-ruby', '~> 5.7'
+gem 'kaminari', '~> 1.1', '>= 1.1.1'
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com",
+@admin_user = AdminUser.create(email: "admin@test.com",
                   password: "123456",
                   password_confirmation: "123456",
                   last_name: "Cashman",
@@ -25,8 +25,18 @@ end
 
 puts "100 Posts created"
 
+100.times do |post|
+  Post.create!(date: Date.today,
+                rationale: "This is my rationale content ##{post}",
+                user_id: @admin_user.id,
+                overtime_request: 2.5)
+end
+
+puts "100 Admin Posts created"
+
 100.times do |audit_log|
   AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 AuditLogs created"
+
