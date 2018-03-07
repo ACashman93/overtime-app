c6b5439f48c0d547d39602e757e658036dd6113d
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-03-07 08:37:30 -0800

    Implemented status enum for the audit log and styled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,22 @@ module ApplicationHelper
     "active" if current_page?(path)
   end
 
+  def status_label_helper status
+    status_span_generator status
+  end
+
+  private
+
+    def status_span_generator status
+      case status
+      when "pending"
+        content_tag(:span, status.titleize, class: 'label label-primary')
+      when "approved"
+        content_tag(:span, status.titleize, class: 'label label-success')
+      when "rejected"
+        content_tag(:span, status.titleize, class: 'label label-danger')
+      when "confirmed"
+        content_tag(:span, status.titleize, class: 'label label-success')
+      end
+    end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,18 +1,2 @@
 module PostsHelper
-  def status_label_helper status
-    status_span_generator status
-  end
-
-  private
-
-    def status_span_generator status
-      case status
-      when "pending"
-        content_tag(:span, status.titleize, class: 'label label-primary')
-      when "approved"
-        content_tag(:span, status.titleize, class: 'label label-success')
-      when "rejected"
-        content_tag(:span, status.titleize, class: 'label label-danger')
-      end
-    end
 end
