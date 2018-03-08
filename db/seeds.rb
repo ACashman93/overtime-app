0e7adda56879f9c66202c6a90efb7b1e0bcdf3e7
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-03-08 09:38:25 -0800

    Implemented callback to update audit log when post is created

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ puts "1 User created"
 
 puts "1 AdminUser created"
 
-100.times do |post|
+50.times do |post|
   Post.create!(date: Date.today,
                 rationale: "This is my rationale content ##{post}. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                 user_id: @user.id,
@@ -25,7 +25,7 @@ end
 
 puts "100 Posts created"
 
-100.times do |post|
+50.times do |post|
   Post.create!(date: Date.today,
                 rationale: "This is my rationale content ##{post}",
                 user_id: @admin_user.id,
