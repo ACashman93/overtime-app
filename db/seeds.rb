896022bbd9424e9ab7df12b6bfca1274709a1e7c
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-03-08 12:57:54 -0800

    Reupdated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,12 @@ puts "1 User created"
 
 puts "1 AdminUser created"
 
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 AuditLogs created"
+
 50.times do |post|
   Post.create!(date: Date.today,
                 rationale: "This is my rationale content ##{post}. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -25,11 +31,6 @@ end
 
 puts "100 Posts created"
 
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 AuditLogs created"
 
 50.times do |post|
   Post.create!(date: Date.today,
